98939ec69251fa7d50938b83de24a1ad554e6025
Ting Luo <tin831@gmail.com>
2019-12-30 08:18:33 -0800

    started

---
+++ data_structures/fizz_buzz_tree/fizz_buzz_tree.py
@@ -0,0 +1,19 @@
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.left = None
+        self.right = None
+
+
+class BinaryTree:
+    def __init__(self):
+        self.root = None
+
+
+def fizz_buzz(value):
+    if value % 15 == 0:
+        return "FizzBuzz"
+    if value % 3 == 0:
+        return "Fizz"
+    if value % 5 == 0:
+        return "Buzz"
