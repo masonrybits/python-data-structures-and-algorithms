b6a050b232c28b2e46df52dbd283c7e1b133163f
Ting Luo <tin831@gmail.com>
2020-01-02 07:43:53 -0800

    finished find_max method & tested

---
+++ data_structures/find_maximum_binary_tree/README.md
@@ -0,0 +1,22 @@
+# Queues with Stacks
+Challenges that utlize trees and queues.
+
+## Challenge Description
+Create a find_maximum_binary_tree method
+
+Define a method that takes in a binary tree and output the maximum value using the breadth_first approach.
+
+
+## Approach & Efficiency
+
+Big O for breadth_first methods:
+* Time: O(n)
+* Space: O(n)
+
+## API
+
+The BinaryTree class has method find_maximum_binary_tree.
+
+## Solution
+![whiteboard](../../assets/find_maximum_binary_tree.jpg)
+
---
+++ data_structures/find_maximum_binary_tree/find_maximum_binary_tree.py
@@ -0,0 +1,79 @@
+from collections import deque
+
+
+class BinaryTree:
+    """Simple BinaryTree with enough functionality for breadth first adding"""
+
+    def __init__(self):
+        self._root = None
+
+    def add(self, value):
+
+        node = _Node(value)
+
+        if not self._root:
+            self._root = node
+            return
+
+        q = Queue()
+
+        q.enqueue(self._root)
+
+        while not q.is_empty():
+
+            current = q.dequeue()
+
+            if current.left:
+                q.enqueue(current.left)
+            else:
+                current.left = node
+                break
+
+            if current.right:
+                q.enqueue(current.right)
+            else:
+                current.right = node
+                break
+
+    def find_maximum_binary_tree(self):
+        if not self._root:
+            return None
+        max_val = 0
+        q = Queue()
+        q.enqueue(self._root)
+        while not q.is_empty():
+            current = q.dequeue()
+            if current.value > max_val:
+                max_val = current.value
+            if current.left:
+                q.enqueue(current.left)
+            if current.right:
+                q.enqueue(current.right)
+        return max_val
+
+
+class _Node:
+    """Protected Tree Node"""
+    def __init__(self, value, left=None, right=None):
+        self.value = value
+        self.left = left
+        self.right = right
+
+
+class Queue:
+    """Implementation of Queue that "composes" built in deque class"""
+
+    def __init__(self):
+        self._dq = deque()
+
+    def enqueue(self, value):
+        self._dq.appendleft(value)
+
+    def dequeue(self):
+        return self._dq.pop()
+
+    def peek(self):
+        return self._dq[-1]
+
+    def is_empty(self):
+        return len(self._dq) == 0
